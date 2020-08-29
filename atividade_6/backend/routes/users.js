b8e1d63da3e4214543f68abbd0e32149c29ffcdc
Wellington <wellingtonsi@alu.ufc.br>
2020-08-29 14:41:28 -0300

    React + Express

---
+++ atividade_6/backend/routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.json(
+    {
+      users: [
+        { name: 'Timmy' },
+        { name: 'Jimmy' },
+        { name: 'Billy' }
+      ]
+    }
+  );
+});
+
+module.exports = router;
