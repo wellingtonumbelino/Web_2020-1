d340f2f662127eaa83a4640ee8e930e65074036b
Wellington <wellingtonsi@alu.ufc.br>
2020-10-10 17:07:43 -0300

    Finalizando atividade 10 sem Redux

--- atividade_10/src/App.js
+++ atividade_10/src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import './App.css';
 
+//imports locais
+import './App.css';
 import Navigate from './components/Navigate';
 import Maior from './components/Maior';
 import Multiplicacao from './components/Multiplicacao';
 import Soma from './components/Soma';
 
+//criando classe
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { num1: 5, num2: 2 }
+    this.state = { num1: '', num2: '' }
+    this.setNovoNum1 = this.setNovoNum1.bind(this);
+    this.setNovoNum2 = this.setNovoNum2.bind(this);
   }
 
+  //criando os set para alterar o valor de num1 e num2
+  setNovoNum1(novoNum1) {
+    this.setState({ num1: novoNum1 });
+  }
+
+  setNovoNum2(novoNum2) {
+    this.setState({ num2: novoNum2 });
+  }
+
+  //render
   render() {
     return (
       <div className='container'>
         <h1>Atividade 10</h1>
         <div className='line'>
-          <Navigate num1={this.state.num1} num2={this.state.num2} />
+          {/* Navigate, para enviar as informações */}
+          <Navigate
+            // Seta os valores de num1 e num2
+            num1={this.state.num1}
+            num2={this.state.num2}
+            // Passa via props para o pai os valores inseridos pelo usuário
+            setNovoNum1={this.setNovoNum1}
+            setNovoNum2={this.setNovoNum2}
+          />
         </div>
         <div className='line'>
+          {/* Componentes que irão executar as operações */}
           <Soma num1={this.state.num1} num2={this.state.num2} />
           <Multiplicacao num1={this.state.num1} num2={this.state.num2} />
           <Maior num1={this.state.num1} num2={this.state.num2} />
--- atividade_10/src/components/Navigate.jsx
+++ atividade_10/src/components/Navigate.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 
 import Card from './Card';
+import '../styles/Navigate.css';
 
 export default class Navigate extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { num1: '', num2: '' }
+        this.setNum1 = this.setNum1.bind(this);
+        this.setNum2 = this.setNum2.bind(this);
+    }
+
+    setNum1(e) {
+        this.setState({ num1: e.target.value });
+    }
+
+    setNum2(e) {
+        this.setState({ num2: e.target.value });
+    }
+
+    resolve(n1, n2) {
+        this.props.setNovoNum1(n1);
+        this.props.setNovoNum2(n2);
+
+        this.setState({ num1: '', num2: '' });
+    }
+
     render() {
         return (
             <Card title='Navegador'>
-                <div></div>
+                <div className='navigate'>
+                    <input
+                        type='number'
+                        placeholder='Número 1'
+                        value={this.state.num1}
+                        onChange={this.setNum1}
+                    />
+                    <input
+                        type='number'
+                        placeholder='Número 2'
+                        value={this.state.num2}
+                        onChange={this.setNum2}
+                    />
+                    <button className='btn btn-success' onClick={() => this.resolve(this.state.num1, this.state.num2)}>Resolver</button>
+                </div>
             </Card>
         )
     }
